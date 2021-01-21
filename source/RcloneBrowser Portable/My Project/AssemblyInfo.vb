718b6ec374a720ac51db2cb447fecab986315d59
MinorMole <FansubTH.001@gmail.com>
2021-01-21 08:40:04 +0700

    Version 2.0.0.9 | Date 21/01/2021
    - Update rclone v1.53.4
    - Update Rclone Browser 2.0.0.9
    - Update mpv 0.33.0 Stable Release

--- source/RcloneBrowser Portable/My Project/AssemblyInfo.vb
+++ source/RcloneBrowser Portable/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("Portable and easy to use version of RcloneBrowser.")>
 <Assembly: AssemblyCompany("MinorMole")>
 <Assembly: AssemblyProduct("RcloneBrowser Portable")>
-<Assembly: AssemblyCopyright("Copyright © MinorMole 2020")>
+<Assembly: AssemblyCopyright("Copyright © MinorMole 2021")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2.0.0.8")>
-<Assembly: AssemblyFileVersion("2.0.0.8")>
+<Assembly: AssemblyVersion("2.0.0.9")>
+<Assembly: AssemblyFileVersion("2.0.0.9")>
