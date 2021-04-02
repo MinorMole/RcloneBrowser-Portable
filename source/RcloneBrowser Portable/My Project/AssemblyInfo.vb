b3c6dc96455e457c1eb313c6f1b41beb618b7dbe
MinorMole <FansubTH.001@gmail.com>
2021-04-03 01:33:21 +0700

    Version 2.0.0.12 | Date 03/04/2021
    - Update rclone v1.55.0
    - Update Rclone Browser 2.0.0.12

--- source/RcloneBrowser Portable/My Project/AssemblyInfo.vb
+++ source/RcloneBrowser Portable/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2.0.0.11")>
-<Assembly: AssemblyFileVersion("2.0.0.11")>
+<Assembly: AssemblyVersion("2.0.0.12")>
+<Assembly: AssemblyFileVersion("2.0.0.12")>
